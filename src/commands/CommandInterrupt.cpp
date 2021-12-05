21908b2cbefbf518e36c1a8fe38579575b7be098
Andrii Sydorenko <andrii.sydorenko@developex.net>
2021-12-05 23:14:40 +0200

    Added multithreading execute of commands
    Added command worker
    Added command stop and interrupt of execute command
    Added auto test for worker

---
+++ src/commands/CommandInterrupt.cpp
@@ -0,0 +1,12 @@
+#include "CommandInterrupt.h"
+
+#include "../types/ExceptionError.h"
+
+namespace otg {
+
+Result<void> CommandInterrupt::execute()
+{
+	return makeUnexpected(ExceptionErrorType::ExecuteInterrupt, "Interrupt execute commands");
+}
+
+}  // namespace otg
